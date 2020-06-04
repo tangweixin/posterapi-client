76a1d3e9b836fd16ce581d3c06ac1c13988ead35
thomas <thomastangweixin@163.com>
2020-06-04 18:54:12 +0800

    示例代码更新

--- src/test/java/cn/prodapi/poster/Test2.java
+++ src/test/java/cn/prodapi/poster/Test2.java
@@ -1,23 +1,21 @@
 package cn.prodapi.poster;
-
 import java.util.HashMap;
 
-public class PosterClientDemo {
+public class Test2 {
 
     public static void main(String[] args) {
 
         // 创建海报客户端对象
 //        PosterClient posterClient = new PosterClient("https://poster.prodapi.cn/", "2NAPIFheaXyL4d2r", "J5GlMp9mzqkTgAiu");
-        PosterClient posterClient = new PosterClient("http://127.0.0.1:9001", "KWmJm7nLTCexrHx0", "jOxt7B5R26jZ3l3yw3pS");
+        PosterClient posterClient = new PosterClient("http://127.0.0.1:9001", "2NAPIFheaXyL4d2r", "J5GlMp9mzqkTgAiu");
 
-        //
+        // 构造海报参数
         HashMap<String, String> params = new HashMap<>();
-        params.put("qrcode", "http://demo.qn.prodapi.cn/images/qiniu.png");
-        params.put("codeNo", "4300002020052022640018");
-        params.put("logo", "/storage/upload/d37e42880dabaf44.png");
+        params.put("文本1", "在世间自有山比此山更高。");
+        params.put("文本2", "在我心世间始终你好");
 
         // 海报ID
-        String posterId = "1";
+        String posterId = "13";
 
         // 获取下载地址
         String url = posterClient.getUrl(posterId, params);
